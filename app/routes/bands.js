dc29c9328b7c889c453105a1fa854adad9245943
Tung Ling Lin <tlin108@hotmail.com>
2016-06-23 12:09:35 -0400

    finish nested routes

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 var Band = Ember.Object.extend({
   name: '',
+
+  slug: Ember.computed('name', function() {
+    return this.get('name').dasherize();
+  }),
 });
 
 var Song = Ember.Object.extend({
