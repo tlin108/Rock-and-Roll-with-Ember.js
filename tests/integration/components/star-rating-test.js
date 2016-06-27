1c8ec3586582af28b365d4dcdb52904a66536f90
Tung Ling Lin <tlin108@hotmail.com>
2016-06-27 11:33:03 -0400

    finish testing

--- tests/integration/components/star-rating-test.js
+++ tests/integration/components/star-rating-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -5,20 +6,26 @@ moduleForComponent('star-rating', 'Integration | Component | star rating', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('Renders the full and empty stars correctly', function(assert) {
+  assert.expect(6);
 
-  this.render(hbs`{{star-rating}}`);
+  var song = Ember.Object.create({ rating: 4 });
+  this.set('song', song);
+  this.set('maxRating', 5);
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{star-rating item=song rating=song.rating maxRating=maxRating}}`);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#star-rating}}
-      template block text
-    {{/star-rating}}
-  `);
+  assert.equal(this.$('.glyphicon-star').length, 4, 'The right amount of full stars is rendered');
+  assert.equal(this.$('.glyphicon-star-empty').length, 1, 'The right amount of empty stars is rendered');
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('maxRating', 10);
+
+  assert.equal(this.$('.glyphicon-star').length, 4, 'The right amount of full stars is rendered after changing maxRating');
+  assert.equal(this.$('.glyphicon-star-empty').length, 6, 'The right amount of empty stars is rendered after changing maxRating');
+
+  this.set('song.rating', 2);
+
+  assert.equal(this.$('.glyphicon-star').length, 2, 'The right amount of full stars is rendered after changing rating');
+  assert.equal(this.$('.glyphicon-star-empty').length, 8, 'The right amount of empty stars is rendered after changing rating');
+
 });
